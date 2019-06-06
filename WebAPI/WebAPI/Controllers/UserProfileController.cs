cf4eeda5fc1725c28387987a661edb3279cb2930
Sajith <43513034+SajithTS@users.noreply.github.com>
2019-06-06 19:34:36 +0530

    Add files via upload

---
+++ WebAPI/WebAPI/Controllers/UserProfileController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UserProfileController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        [Authorize]
+        public async Task<Object> GetUserProfile()
+        {
+            string userID = User.Claims.First(x => x.Type == "UserId").Value;
+            var user = await _userManager.FindByIdAsync(userID);
+            return new
+            {
+                user.FullName,
+                user.Email,
+                user.UserName
+            };
+        }
+    }
+}
